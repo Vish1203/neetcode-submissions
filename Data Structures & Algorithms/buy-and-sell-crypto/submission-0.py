b4de09ccf2d49728470fbda75db3d224e0f168f5
VISHAKHA GAUTAM <32431308+Vish1203@users.noreply.github.com>
2026-09-25 23:32:00 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # O(n^2) solution
+        maxP = 0
+        for i in range(len(prices)):
+            for j in range(i + 1, len(prices)):
+                profit = prices[j] - prices[i]
+                if profit > maxP:
+                    maxP = profit 
+        
+        return maxP
